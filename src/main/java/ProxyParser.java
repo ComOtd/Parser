f4148f2fc3430a11c5dce77436cbff7218fce94d
Семён <comotd@gmail.com>
2019-05-27 00:07:52 +0300

    ProxyParser home

--- src/main/java/ProxyParser.java
+++ src/main/java/ProxyParser.java
@@ -2,10 +2,10 @@ import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
+
 
 import java.io.IOException;
-import java.util.regex.Pattern;
+
 
 public class ProxyParser {
 
@@ -23,7 +23,7 @@ public class ProxyParser {
             String HTTP = text.substring(text.indexOf("<h2>HTTP</h2>"), text.indexOf("<h2>SOCKS</h2>"))
                     .replaceAll("<h2>HTTP[S]?</h2>|<br>","");
 
-          //System.out.println(HTTP);
+          System.out.println(HTTP);
 
 
             Document doc2 = Jsoup.connect("http://free-proxy-server.net/")
